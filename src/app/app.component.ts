015e6373e2f5c4a3b338d638b6d26ad722cae207
Amara-ahmed <ahmed.amara606@gmail.com>
2024-03-18 08:40:44 +0100

    feat/changesOnTheFront

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';// import et telechargement de metadonnées
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',//metadonnées qui contiennent le template html + css
+
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'elective-web-frontend';
+  title = 'Elective-web-frontend';
 }
+
