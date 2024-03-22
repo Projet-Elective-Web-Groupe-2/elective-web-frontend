7795cff9b6a8c0febb56d9c072b936b77a5eaf66
PaulBos <paul.bosacki@viacesi.fr>
2024-03-22 11:20:28 +0100

    [feat] swap to Angular 15.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elective-web-frontend';
+
+
+  /*initMap() {
+    this.map = L.map('map').setView([48.8582599, 2.2945006358633115], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+  }*/
 }
