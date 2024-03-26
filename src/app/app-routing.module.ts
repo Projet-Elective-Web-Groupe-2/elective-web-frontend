10926615439d15ea03d5d77cea8a28023c04202b
PaulBos <paul.bosacki@viacesi.fr>
2024-03-26 09:29:29 +0100

    feat(routing):create all main components + header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { SignupComponent } from './signup/signup.component';
+import { ClientComponent } from './client/client.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { DeveloperComponent } from './developer/developer.component';
+import { SalesComponent } from './sales/sales.component';
 
 
 const routes: Routes = [{
@@ -9,10 +14,11 @@ const routes: Routes = [{
 },
 {path: '', redirectTo: 'auth', pathMatch: 'full'},
 {path: 'signup', component: SignupComponent},
-//{path: 'tracker', component: FlowMonitoringComponent,},
-
-
-//{path: 'detail-desadv/:id', component: DetailDesadvComponent},
+{path: 'client', component: ClientComponent},
+{path: 'restaurant', component: RestaurantComponent},
+{path: 'delivery', component: DeliveryComponent},
+{path: 'developer', component: DeveloperComponent},
+{path: 'sales', component: SalesComponent},
 ]
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,18 @@
 import { APP_INITIALIZER, LOCALE_ID, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [],
-  exports: [],
+  declarations: [
+      HeaderComponent
+    ],
+  imports: [
+      RouterModule,
+      CommonModule,
+    ],  
+  exports: [HeaderComponent],
   providers: [],
+
 })
 export class CoreModule {}
