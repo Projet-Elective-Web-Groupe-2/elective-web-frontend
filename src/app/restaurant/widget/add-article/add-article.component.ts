37d7d6ad85fa5699386a5741a3cafe1777e08fdb
PaulBos <paul.bosacki@viacesi.fr>
2024-04-04 13:47:13 +0200

    feat(restaurant): create article

--- src/app/restaurant/widget/add-article/add-article.component.ts
+++ src/app/restaurant/widget/add-article/add-article.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-article',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-article.component.css']
 })
 export class AddArticleComponent {
+  selectedImage: string | ArrayBuffer | null | undefined;
+  
+  constructor(private formBuilder: FormBuilder) {}
+  
+  articleForm = this.formBuilder.group({
+    name: new FormControl(),
+    compo: new FormControl(),
+    price: new FormControl('0'),
+    photo: new FormControl(''),
+  });
 
+  ngOnInit(): void {
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  submitArticle(){
+    console.log(this.articleForm.value);
+  }
 }
--- src/app/restaurant/widget/add-menu/add-menu.component.ts
+++ src/app/restaurant/widget/add-menu/add-menu.component.ts
@@ -18,7 +18,7 @@ export class AddMenuComponent implements OnInit {
     price: new FormControl('0'),
     drink: new FormControl(false),
     photo: new FormControl(''),
-  });//https://eu-images.contentstack.com/v3/assets/blt5004e64d3579c43f/blt9418bc6e38e6544a/660439b26eb29729ab905cdf/MxBO_BIGMAC.png?auto=webp&width=1280&disable=upscale
+  });
 
   ngOnInit(): void {
     this.articleList.push('Big Muc');
