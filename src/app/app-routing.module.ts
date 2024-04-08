6b73571a33ec20a427f30e3ce3942442f03b9ac0
Frogounet <thefrogman59@gmail.com>
2024-04-08 10:54:31 +0200

    feat(route): add error page and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [{
 {path: 'sales', component: SalesComponent},
 
 {path: 'error-page', component: ErrorPageComponent},
+{path: '**', component: ErrorPageComponent},
 
 
 {path: 'restaurant/add-menu', component: AddMenuComponent},
--- src/app/core/components/error-page/error-page.component.ts
+++ src/app/core/components/error-page/error-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./error-page.component.css']
 })
 export class ErrorPageComponent {
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    // Redirection vers une autre page après un délai de 5 secondes
+    setTimeout(() => {
+      this.router.navigate(['/home']); // Remplacez '/home' par la route de votre choix
+    }, 5000);
+  }
 }
