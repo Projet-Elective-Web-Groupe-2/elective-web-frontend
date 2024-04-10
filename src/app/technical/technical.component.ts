19ed7ac61f9ab5e039fc1042980fa8300be199e9
Frogounet <thefrogman59@gmail.com>
2024-04-10 11:29:13 +0200

    feat(technical): link with back

--- src/app/technical/technical.component.ts
+++ src/app/technical/technical.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LogService } from '../core/services/logs.service';
+import { SessionStorageService } from '../core/services/session-storage.service';
+import { HttpResponse } from '@angular/common/http';
+import { LogModel } from '../core/models/log.model';
 
 @Component({
   selector: 'app-technical',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class TechnicalComponent {
 
+  logs:string[]=[];
+  token!:any;
+  log!:any;
+  constructor(private router: Router,private logservice: LogService,private sessionStorage : SessionStorageService) { }
+
+  ngOnInit(){
+    this.token = this.sessionStorage.getItem("token");
+    this.logs.push("log1");
+    this.logs.push("log2");
+    this.logs.push("log3");
+    this.logs.push("log4");
+    this.getLog();
+  }
+
+  getLog(){
+    this.log = this.logservice.getLog(this.token).subscribe((response: LogModel) => {
+      this.logs= response.logs;
+    });
+    //console.log(this.log);
+  }
+
+  onClick(){
+    console.log("test click");
+    window.location.href = "http://localhost:8080";
+  }
 }
