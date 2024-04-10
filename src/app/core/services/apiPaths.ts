97e7ae94af7bdc57c5a8d147610c9b00b6adc0d6
Amara-ahmed <ahmed.amara606@gmail.com>
2024-04-10 15:44:45 +0200

    fucking commit

--- src/app/core/services/apiPaths.ts
+++ src/app/core/services/apiPaths.ts
@@ -14,11 +14,11 @@ export const API = Object.freeze({
 
     getOrder: '/order/getOrder',
     getOrders: '/order/getOrders',
+    getOrderCount: '/order/getOrderCount',
 
-    createMenu:'/menu/create',
     createArticle:'/product/add',
 
-    getRestaurantInformation: '/restaurant/findByOwner',
+
     getRestaurant:'/restaurant/getAll',
     getRestaurantDetail:'/restaurant/find',
 
---
+++ src/app/core/services/notifications.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API } from './apiPaths';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NotificationsService {
+    constructor(private http: HttpClient) { }
+
+    getOrderCount(token: any, userID: any): Observable<any> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            })
+        };
+
+        const params = new HttpParams().set('restaurantID', userID);
+
+        return this.http.get(`${environment.urlRestaurant}${API.getOrderCount}`, { params, ...httpOptions });
+    }
+}
