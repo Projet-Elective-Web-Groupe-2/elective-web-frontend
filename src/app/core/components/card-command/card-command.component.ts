5b7a8152c792e01c300b004d414886611a16716b
PaulBos <paul.bosacki@viacesi.fr>
2024-04-12 09:05:15 +0200

    fix(): getOrders for restaurant

--- src/app/core/components/card-command/card-command.component.ts
+++ src/app/core/components/card-command/card-command.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Orders } from '../../models/infoOrders.model';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { OrderService } from '../../services/order.service';
+import { SessionStorageService } from '../../services/session-storage.service';
+import { MessageModel } from '../../models/message.model';
 
 @Component({
   selector: 'app-card-command',
@@ -8,9 +13,55 @@ import { Orders } from '../../models/infoOrders.model';
 })
 export class CardCommandComponent  implements OnInit {
 
+  token!:any;
+  restaurantID!:any;
   @Input() OrderValues!: Orders;
 
+
+  constructor(
+    private sessionStorageService: SessionStorageService,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    private orderService: OrderService,
+  ) { }
+
+  
   ngOnInit() {
    console.log(this.OrderValues);
+  }
+
+
+  acceptCommand(){
+    this.token = this.sessionStorageService.getItem('token');
+    this.restaurantID = this.sessionStorageService.getItem('restaurantID');
+    this.orderService.updateOrder(this.token,this.restaurantID,this.OrderValues.id,this.OrderValues.status).subscribe({
+      next: (response: MessageModel) => {
+        if(response.message == "Order status updated"){
+          this.toastr.success("Commande validée");
+        }
+        else{
+          this.toastr.success("Erreur lors de la validation de la commande");
+        }
+      },
+      error: () => {
+        this.toastr.error("Erreur lors de la récupération des commandes");
+      }
+    });
+  }
+
+  refuseCommand(){
+    this.orderService.updateOrder(this.token,this.restaurantID,this.OrderValues.id,'Order refused by restaurateur').subscribe({
+      next: (response: MessageModel) => {
+        if(response.message == "Order status updated"){
+          this.toastr.success("Commande refusé");
+        }
+        else{
+          this.toastr.success("Erreur lors du refus de la commande");
+        }
+      },
+      error: () => {
+        this.toastr.error("Erreur lors de la récupération des commandes");
+      }
+    });
   }
 }
